48ecd4ec711c5cbba0bb8c8346afd1be3cecff30
HallexCosta <hallex.costa@hotmail.com>
2021-09-05 17:28:23 -0300

    add linting and change title

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,12 +1,11 @@
-const User = require("./User");
+const User = require('./User')
 
 module.exports = {
   Query: {
     users: () => User.find(),
-    user: (_, { id }) => User.findById(id),
+    user: (_, { id }) => User.findById(id)
   },
   Mutation: {
-    createUser: (_, { name, email }) => User.create({ name, email }),
-  },
-};
-
+    createUser: (_, { name, email }) => User.create({ name, email })
+  }
+}
